5cc623d9ef2a5f903067beb1fa5b35b9f6b8bf1c
Tobias Ranft <develop@ranft.biz>
2015-05-01 22:53:29 +0200

    Preparation for phpmetrics

--- setup/run.docker.sh
+++ setup/run.docker.sh
@@ -3,5 +3,6 @@ docker run -p 81:80 -it -d --name my-apache-php-app \
  -v "$PWD/src:/var/www/src:ro" \
  -v "$PWD/public:/var/www/html:ro" \
  -v "$PWD/view:/var/www/view:ro" \
-  -v "$PWD/application:/var/www/application:ro" \
-php:5.6-apache
+ -v "$PWD/application:/var/www/application:ro" \
+ -v "$PWD/build:/var/www/build:ro" \
+ php:5.6-apache
