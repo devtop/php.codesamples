c1976337ad9837aa5938996b5a759fce6ca16b89
Tobias Ranft <develop@ranft.biz>
2015-05-02 08:07:00 +0200

    Added offline bootstrap and jQuery version for development

--- setup/run.docker.sh
+++ setup/run.docker.sh
@@ -5,4 +5,5 @@ docker run -p 81:80 -it -d --name my-apache-php-app \
  -v "$PWD/view:/var/www/view:ro" \
  -v "$PWD/application:/var/www/application:ro" \
  -v "$PWD/build:/var/www/build:ro" \
+ -v "$PWD/offline:/var/www/offline:ro" \
  php:5.6-apache
