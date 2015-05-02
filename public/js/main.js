f8eef6d626dc9c6497b0582012ad847533f10199
Tobias Ranft <develop@ranft.biz>
2015-05-02 20:19:55 +0200

    Added hidden email address.

---
+++ public/js/main.js
@@ -0,0 +1,5 @@
+/**
+ * Created by Tobias Ranft
+ */
+
+$( "span.insertmail" ).html("<a href=\"mailto:"+"coded"+String.fromCharCode(64)+"ranft.biz\">&lt;coded"+String.fromCharCode(64)+"ranft.biz&gt;</a>");
